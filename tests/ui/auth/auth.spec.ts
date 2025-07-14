adab4574aed908e0aca425ddc18bc0e415c48b51
IgnisMongrel <illia.i.ihnatov@gmail.com>
2025-07-14 15:25:38 +0300

    fix formatting 3

--- tests/ui/auth/auth.spec.ts
+++ tests/ui/auth/auth.spec.ts
@@ -2,7 +2,7 @@ import { Locator, expect, test } from '@playwright/test'
 import { SERVICE_URL } from '../../../config/env-data'
 import { LoginPage } from '../../pages/login-page'
 
-test.only('Sign in button is disabled when an invalid username is entered', async ({ page }) => {
+test('Sign in button is disabled when an invalid username is entered', async ({ page }) => {
   await page.goto(SERVICE_URL)
   const usernameField = page.getByTestId('username-input')
   await usernameField.fill('-')
